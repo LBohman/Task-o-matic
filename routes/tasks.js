19e4fa583dd738fb8e902ba49e5c078988b8540e
Lovisa Bohman <lovisabo@hotmail.com>
2021-02-07 12:54:59 +0100

    Set up routes

--- routes/tasks.js
+++ routes/tasks.js
@@ -5,4 +5,27 @@ router.get('/tasks', (req, res) => {
     res.send('in tasks')
 });
 
+router.post('/new', (req, res) => {
+    const tasks = [{
+        task: "Remove nailpolish",
+        createdAt: new Date
+    },
+    {
+        task: "Press submit and post to /new",
+        createdAt: new Date
+    }]
+    res.render('index', { tasks: tasks });
+    console.log('Add button clicked');
+});
+
+router.get('/edit', (req, res) => {
+    console.log('Edit link pressed');
+    res.redirect('/');
+});
+
+router.get('/delete', (req, res) => {
+    console.log('Delete link pressed');
+    res.redirect('/');
+});
+
 module.exports = router;
